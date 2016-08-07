626cbcf742f4ad560c5df58e1232d0680e94f067
Paul <mothcar@gmail.com>
2016-08-07 20:37:53 +0900

    Add files via upload

---
+++ LifeController.js
@@ -0,0 +1,18 @@
+/**
+ * LifeController
+ *
+ * @description :: Server-side logic for managing lives
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+var Meaning = require('the‐ultimate‐question'); 
+
+module.exports = {
+	purpose: function(req, res) {
+		return res.json({
+			answer: Meaning.answer(), 
+			question: Meaning.question()
+		});
+	}
+	
+};
+
